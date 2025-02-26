b65de25df720fc03703c48f5aef91b292edcad43
GrimaldoSantiago98 <grimaldotv4@gmail.com>
2025-02-26 12:12:40 +0000

    codigo das cartas cadastradas

--- xadrez.c
+++ xadrez.c
@@ -1,8 +1,46 @@
-#include   <stdio.h> 
+#include <stdio.h>
 
-int main(){
+int main() {
+    
+    // CARTA A
+    
+    char estadoA = 'A';
+    char codigoA[5] = "A01";
+    char Nome_da_cidadeA[20] = "Sao paulo";
+    int populacaoA = 12325000;
+    float area_kmA = 1521.11;
+    float pib_A = 699.28;
+    int numero_de_pontos_turistico_A = 50;
+    
+    printf("Estado: %c\n", estadoA);
+    printf("Codigo: %s\n", codigoA);
+    printf("Nome da cidade: %s\n", Nome_da_cidadeA);
+    printf("Populacao: %i\n", populacaoA);
+    printf("Area km²: %f\n", area_kmA);
+    printf("PIB: %f\n", pib_A);
+    printf("Pontos turisticos: %i\n\n", numero_de_pontos_turistico_A);
+    
+    // CARTA B
+    
+    char estadoB = 'B';
+    char codigoB[5] = "B02";
+    char nome_da_cidadeB[20] = "Rio de janeiro";
+    int populacaoB = 6748000;
+    float area_kmB = 1200.25;
+    float pib_B = 300.50;
+    int numero_de_pontos_turistico_B = 30;
+   
+    printf("Estado: %c\n", estadoB);
+    printf("Codigo: %s\n", codigoB);
+    printf("Nome da cidade: %s\n", nome_da_cidadeB);
+    printf("Populacao: %i\n", populacaoB);
+    printf("Area km²: %f\n,", area_kmB);
+    printf("PIB: %f\n", pib_B);
+    printf("Pontos turisticos: %i\n", numero_de_pontos_turistico_B);
+    
 
-    printf("Super trunfo\n");
-
-
-}
+    
+    
+    
+    
+    return 0;}
